a6f4c6de02c1a448dcd6e95b41cf3ae0999fde24
Adrien Driffaud <adrien@driffaud.fr>
2023-10-14 23:50:10 +0200

    list locally installed INDI drivers

--- cmd/indi-web/main.go
+++ cmd/indi-web/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strings"
 
 	indiserver "github.com/adriffaud/indi-web/internal/indi-server"
+	"github.com/beevik/etree"
 	"github.com/julienschmidt/httprouter"
 )
+
+type Device struct {
+	Name          string
+	Manufacturer  string
+	DriverCaption string
+	DriverName    string
+	Version       string
+}
 
 func Index(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	tmpl, _ := os.ReadFile("web/template/index.html")
@@ -29,6 +41,68 @@ func Index(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func INDIDrivers(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	files, err := os.ReadDir("/usr/share/indi")
+	if err != nil {
+		log.Printf("could not list INDI drivers: %v", err)
+		return
+	}
+
+	groups := make(map[string][]Device)
+
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".xml") {
+			doc := etree.NewDocument()
+			err = doc.ReadFromFile(fmt.Sprintf("/usr/share/indi/%s", file.Name()))
+			if err != nil {
+				log.Printf("could not list INDI drivers: %v", err)
+				return
+			}
+
+			driversList := doc.SelectElement("driversList")
+			if driversList == nil {
+				continue
+			}
+
+			for _, groupElem := range driversList.SelectElements("devGroup") {
+				var drivers []Device
+
+				group := groupElem.SelectAttrValue("group", "")
+
+				if existingDrivers, ok := groups[group]; ok {
+					drivers = append(existingDrivers, drivers...)
+				}
+
+				for _, driver := range groupElem.ChildElements() {
+					driverChild := driver.SelectElement("driver")
+
+					device := Device{
+						Name:          driver.SelectAttrValue("label", ""),
+						Manufacturer:  driver.SelectAttrValue("manufacturer", ""),
+						DriverCaption: driverChild.SelectAttrValue("name", ""),
+						DriverName:    driverChild.Text(),
+						Version:       driver.SelectElement("version").Text(),
+					}
+
+					drivers = append(drivers, device)
+				}
+
+				groups[group] = drivers
+			}
+		}
+	}
+
+	for _, drivers := range groups {
+		sort.Slice(drivers, func(i, j int) bool {
+			return drivers[i].Name < drivers[j].Name
+		})
+	}
+
+	jsonb, _ := json.Marshal(groups)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(jsonb))
 }
 
 func INDIServer(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
@@ -53,6 +127,7 @@ func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.POST("/indi/activate", INDIServer)
+	router.GET("/indi/drivers", INDIDrivers)
 	router.ServeFiles("/static/*filepath", http.Dir("web/static"))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	golang.org/x/sys v0.12.0
 )
+
+require github.com/beevik/etree v1.2.0
