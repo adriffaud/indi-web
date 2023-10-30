d8f5e91756bd78ce6cb082521e025d73d56f4d60
Adrien Driffaud <adrien@driffaud.fr>
2023-10-30 12:54:09 +0100

    simplify xml reading

--- internal/indi-client/client.go
+++ internal/indi-client/client.go
@@ -1,7 +1,6 @@
 package indiclient
 
 import (
-	"bytes"
 	"encoding/xml"
 	"fmt"
 	"log"
@@ -10,11 +9,6 @@ import (
 
 type Client struct {
 	Conn net.Conn
-}
-
-type Message struct {
-	XMLName xml.Name
-	Content string `xml:",innerxml"`
 }
 
 func New(address string) (*Client, error) {
@@ -42,32 +36,19 @@ func (c *Client) GetProperties() error {
 }
 
 func recv(c net.Conn) {
-	buf := make([]byte, 2048)
-	var incompleteData []byte
+	decoder := xml.NewDecoder(c)
 
 	for {
-		n, err := c.Read(buf)
+		t, err := decoder.Token()
 		if err != nil {
-			log.Println("read error:", err)
-			continue
+			break
 		}
 
-		data := append(incompleteData, buf[:n]...)
-		incompleteData = processData(data)
-	}
-}
-
-func processData(data []byte) []byte {
-	decoder := xml.NewDecoder(bytes.NewReader(data))
-
-	for {
-		var msg Message
-		err := decoder.Decode(&msg)
-		if err != nil {
-			return data
+		switch t.(type) {
+		case xml.StartElement:
+			log.Println("============================")
+			log.Printf("%+v\n", t)
+		case xml.EndElement:
 		}
-
-		log.Println("================================")
-		log.Printf("[INDI Client] Received:\n%+v\n", msg)
 	}
 }
