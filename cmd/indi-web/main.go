6b510c63f2bb3a449e862fc23b55f1385b80f0c4
Adrien Driffaud <adrien@driffaud.fr>
2024-05-20 09:30:52 +0200

    allow specifying server host and port

--- cmd/indi-web/main.go
+++ cmd/indi-web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -13,14 +15,23 @@ type application struct {
 	indiClient *indiclient.Client
 }
 
+var (
+	host string
+	port int
+)
+
 func main() {
+	flag.StringVar(&host, "host", "localhost", "server host")
+	flag.IntVar(&port, "port", 8080, "server port")
+	flag.Parse()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	slog.SetDefault(logger)
 
 	app := &application{}
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf("%s:%d", host, port),
 		Handler:      app.routes(),
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 10,
