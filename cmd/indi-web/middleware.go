0630c64c1eadefa1369be50fbfbc3dcffdd0f20d
Adrien Driffaud <adrien@driffaud.fr>
2024-05-24 20:08:56 +0200

    fix static assets loading

--- cmd/indi-web/middleware.go
+++ cmd/indi-web/middleware.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	indiserver "github.com/adriffaud/indi-web/internal/indi-server"
 )
 
 func (app *application) checkServerStarted(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !indiserver.IsRunning() && r.URL.Path != "/setup" {
+		if !indiserver.IsRunning() && r.URL.Path != "/setup" && !strings.HasPrefix(r.URL.Path, "/static/") {
 			http.Redirect(w, r, "/setup", http.StatusTemporaryRedirect)
 			return
 		}
